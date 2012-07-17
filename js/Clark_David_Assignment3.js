ea678146cea39c49713a9cc531548cc8ec170662
Dave Clark <Ackdamos@gmail.com>
2012-07-17 02:17:15 -0400

    Added in global variables.
    The variables listed here are just temporary. Will replace with actual
    variables that pertain to my theme/story.

--- js/Clark_David_Assignment3.js
+++ js/Clark_David_Assignment3.js
@@ -3,3 +3,13 @@
    Assignment 3
    Placeholder text for now.
 */
+
+var tempString = "This is just temporary.",
+	tempNumber = 11,
+	tempArray = [
+		"Temporary",
+		"Array",
+		"Placeholders",
+		"Here"
+	]
+;
