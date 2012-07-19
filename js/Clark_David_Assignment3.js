1c60316b8ac73bc583dea2a27e58d95bd19b54e1
Dave Clark <Ackdamos@gmail.com>
2012-07-19 18:37:23 -0400

    Added another object.
    Inside the object, I made a procedure that has a while loop contained
    in it. Commented part is just a for loop I was testing.

--- js/Clark_David_Assignment3.js
+++ js/Clark_David_Assignment3.js
@@ -9,7 +9,7 @@
 var tempString = "This is just temporary.",
 	goodReport = "Keep up the good work soldier!",
 	badReport = "Your all kinds of messed up!"
-	tempNumber = 11,
+	weaponCount = 20,
 	tempArray = [
 		"Temporary",
 		"Array",
@@ -48,3 +48,23 @@ var key = "name";
 for (var key in soldier) {
 	console.log(key + " = " + soldier[key]);
 }
+
+var officer = {
+	name: "John Williams",
+	age: 38,
+	rank: "Captain",
+	leader: true,
+	countWeapons: function () {
+		while (weaponCount > 0) {
+		console.log("There are " + weaponCount + " weapons left.");
+		weaponCount--;
+		}
+		console.log("All weapons are accounted for.");
+	}
+};
+
+var goLeader = officer.countWeapons();
+
+// for (var i = 0; i <= 20; i++) {
+//		console.log("You " + i + " soldiers are platoon 1.");
+//		} 
